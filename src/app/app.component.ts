3811caea741c8b528a70ae16097b5796fb6b295f
Aleksey Borkovsky <it.borki@gmail.com>
2024-02-22 19:24:18 +0300

    Added animation and adaptive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { AppService } from './app.service';
 
@@ -13,6 +13,9 @@ export class AppComponent {
   loaderShowed = true;
   loader = true;
 
+  mainImageStyle: any;
+  orderImageStyle: any;
+
   form = this.fb.group({
     order: ['', Validators.required],
     name: ['', Validators.required],
@@ -22,6 +25,26 @@ export class AppComponent {
   productsData: any;
 
   constructor(private fb: FormBuilder, private appService: AppService) {}
+
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove(e: MouseEvent) {
+    this.orderImageStyle = {
+      transform:
+        'translate(-' +
+        (e.clientX * 0.3) / 8 +
+        'px,-' +
+        (e.clientY * 0.3) / 8 +
+        'px)',
+    };
+    this.mainImageStyle = {
+      transform:
+        'translate(-' +
+        (e.clientX * 0.3) / 8 +
+        'px,-' +
+        (e.clientY * 0.3) / 8 +
+        'px)',
+    };
+  }
 
   ngOnInit() {
     setTimeout(() => {
